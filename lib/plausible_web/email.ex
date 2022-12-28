57ced7c53fce567d6f05551b84b984c7bca2f00a
Ubuntu <ubuntu@ip-172-26-7-37.ap-northeast-2.compute.internal>
2022-12-28 00:47:36 +0000

    email

--- lib/plausible_web/email.ex
+++ lib/plausible_web/email.ex
@@ -18,7 +18,7 @@ defmodule PlausibleWeb.Email do
     base_email()
     |> to(user)
     |> tag("welcome-email")
-    |> subject("Welcome to Plausible")
+    |> subject("서브키트 애널리틱스 가입을 환영합니다")
     |> render("welcome_email.html", user: user, unsubscribe: true)
   end
 
