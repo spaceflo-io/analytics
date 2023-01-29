642d578c7d5cc1240baa5e5a644f270004494b23
Choi at Spaceflo <77764953+choi-spaceflo@users.noreply.github.com>
2023-01-29 17:30:45 +0900

    reset password

--- lib/plausible_web/email.ex
+++ lib/plausible_web/email.ex
@@ -66,7 +66,7 @@ defmodule PlausibleWeb.Email do
     base_email(%{layout: nil})
     |> to(email)
     |> tag("password-reset-email")
-    |> subject("Plausible password reset")
+    |> subject("서브키트 애널리틱스 비밀번호 설정")
     |> render("password_reset_email.html", reset_link: reset_link)
   end
 
